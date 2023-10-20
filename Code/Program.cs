60ed8bf8be6304d52ffcf88ea1ff7d7de5805df3
MarkusLinV <maksim.lin.00@mail.ru>
2023-10-20 18:48:26 +0200

    Написал и протестировал решение в файле Programm.cs

--- Code/Program.cs
+++ Code/Program.cs
@@ -10,19 +10,32 @@ void FillArrayFromUser(string[] array)
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"Введите {i} элемент массива: ");
-        array[i] = Console.ReadLine();
+        array[i] = Console.ReadLine()!;
     }
 }
 
-void PrintArray(int[] array)
+void PrintArray(string[] array)
 {
 for (int i = 0; i < array.Length; i++)
 {
-    Console.Write($"{array[i]}; ");
+    Console.Write($"{array[i]} ");
 }  
 }
 
+string[] array = new string[GetNumber("длинны массива array")];
+string[] resault = new string[array.Length];
 
+FillArrayFromUser(array);
+PrintArray(array);
+int n = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        resault[n] = array[i];
+        n = n + 1;
+    }
+}
 
-
-
+Console.WriteLine();
+PrintArray(resault);
