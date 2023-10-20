f47700b5e4a48b834bedf19272bda109eb4476de
MarkusLinV <maksim.lin.00@mail.ru>
2023-10-20 18:29:02 +0200

    Добавил необходимые методы

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿int GetNumber(string message)
+{
+    Console.Write($"Введите число {message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void FillArrayFromUser(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i} элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+}
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}; ");
+}  
+}
+
+
+
+
+
